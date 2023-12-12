b2bb4c77aacaf2676c20477342a9b7301190a127
ELIASHEAD <eliashead.costa@gmail.com>
2023-12-12 14:50:41 -0300

    page project details

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import { ProjectDtails } from '@/app/components/pages/project/project-details'
+
+function Project() {
+  return (
+    <>
+      <ProjectDtails />
+    </>
+  )
+}
+
+export default Project
