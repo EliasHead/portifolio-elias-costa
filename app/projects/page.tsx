43d8e6e82d28219c952e76817a1a323d9242f0e5
ELIASHEAD <eliashead.costa@gmail.com>
2023-12-12 10:16:40 -0300

    projects list

---
+++ app/projects/page.tsx
@@ -0,0 +1,13 @@
+import { PageIntroduction } from '../components/pages/projects/page-introduction'
+import { ProjectsList } from '../components/pages/projects/projects-list'
+
+const Project = () => {
+  return (
+    <>
+      <PageIntroduction />
+      <ProjectsList />
+    </>
+  )
+}
+
+export default Project
