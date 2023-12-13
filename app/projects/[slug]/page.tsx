b4bd0d01afdf18edeb21978487ce09b87f4e3ef1
ELIASHEAD <eliashead.costa@gmail.com>
2023-12-13 11:31:29 -0300

    creditials Hygraph

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import { ProjectDtails } from '@/app/components/pages/project/project-details'
+import { ProjectSections } from '@/app/components/pages/project/project-sections'
 
 function Project() {
   return (
     <>
       <ProjectDtails />
+      <ProjectSections />
     </>
   )
 }
