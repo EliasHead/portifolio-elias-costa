f6f61212c10ccb2ebed4d938f647fd079c799e72
ELIASHEAD <eliashead.costa@gmail.com>
2023-12-15 11:50:24 -0300

    Porject details

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,13 +1,59 @@
 import { ProjectDtails } from '@/app/components/pages/project/project-details'
 import { ProjectSections } from '@/app/components/pages/project/project-sections'
+import { ProjectPageData } from '@/app/types/page-info'
+import { fetchHygraphQuery } from '@/app/utils/fetch-hygraph-query'
 
-function Project() {
+type ProjectProps = {
+  params: {
+    slug: string
+  }
+}
+
+const getProjectDetails = async (slug: string): Promise<ProjectPageData> => {
+  const query = `
+  query ProjectQuery() {
+    project(where: {slug: "${slug}"}) {
+      pageThumbnail {
+        url
+      }
+      thumbnail {
+        url
+      }
+      sections {
+        title
+        image {
+          url
+        }
+      }
+      title
+      shortDescription
+      description {
+        raw
+        text
+      }
+      technologies {
+        name
+      }
+      liveProjectUrl
+      githubUrl
+    }
+  }
+  `
+  const data = fetchHygraphQuery(
+    query,
+    1000 * 60 * 60 * 24, // 1 day
+  )
+
+  return data
+}
+
+export default async function Project({ params: { slug } }: ProjectProps) {
+  const { project } = await getProjectDetails(slug)
+
   return (
     <>
-      <ProjectDtails />
-      <ProjectSections />
+      <ProjectDtails project={project} />
+      <ProjectSections sections={project.sections} />
     </>
   )
 }
-
-export default Project
