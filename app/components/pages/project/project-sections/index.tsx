aa60361f7511744f28d393c583becc1827214a9c
ELIASHEAD <eliashead.costa@gmail.com>
2023-12-19 15:25:01 -0300

    feat: final animete

--- app/components/pages/project/project-sections/index.tsx
+++ app/components/pages/project/project-sections/index.tsx
@@ -1,5 +1,8 @@
+'use client'
 import { ProjectSection } from '@/app/types/projects'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
+import { fadeUpAnimation } from '@/app/lib/animations'
 
 type ProjectSectionProps = {
   sections: ProjectSection[]
@@ -9,9 +12,11 @@ export const ProjectSections = ({ sections }: ProjectSectionProps) => {
   return (
     <section className="container my-12 md:my-32 flex flex-col gap-8 md:ga-32">
       {sections.map((section) => (
-        <div
+        <motion.div
           key={section.title}
           className="flex flex-col items-center gap-6 md:gap-12"
+          {...fadeUpAnimation}
+          transition={{ duration: 0.5 }}
         >
           <h2 className="text-2xl md:text-3xl font-medium text-gray-300">
             {section.title}
@@ -24,7 +29,7 @@ export const ProjectSections = ({ sections }: ProjectSectionProps) => {
             alt={`Imagem da sessão ${section.title}`}
             unoptimized
           />
-        </div>
+        </motion.div>
       ))}
     </section>
   )
